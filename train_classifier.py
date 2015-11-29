e8ee60fca944343ada4457ab97f7601ce737dc2b
Yixing Lao <linuxthink@gmail.com>
2015-11-29 12:24:21 -0800

    iteration to 500

--- train_classifier.py
+++ train_classifier.py
@@ -14,15 +14,15 @@ from feature_label_builder import data_path, full_path
 
 (train_xs, train_ys) = pickle.load(open(full_path("train_xs_ys_np.p"), "rb"))
 
-classifier = GBC(n_estimators=2000,
-                 learning_rate=0.1,
+classifier = GBC(n_estimators=500,
+                 learning_rate=0.15,
                  subsample=0.001,
                  max_features='auto',
                  min_samples_leaf=9,
                  verbose=1)
 classifier.fit(train_xs, train_ys)
 
-pickle.dump(classifier, open(full_path("classifier_2000_0.1_0.001_9.p"), "wb"))
+pickle.dump(classifier, open(full_path("classifier_500_0.1_0.001_9.p"), "wb"))
 
 # keys = get_feature_keys()
 # for key, val in zip(keys, classifier.feature_importances_):
