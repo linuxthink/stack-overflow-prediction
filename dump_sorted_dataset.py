3f2f0a7f23e9d38c0e3bfaf492372126a75b5fa5
Yixing Lao <linuxthink@gmail.com>
2015-11-30 10:35:50 -0800

    dump sorted dataset 37

--- dump_sorted_dataset.py
+++ dump_sorted_dataset.py
@@ -12,15 +12,15 @@ import scipy as sp
 from feature_label_builder import FeatureLabelBuilder, get_feature_keys
 from feature_label_builder import data_path, full_path
 
-# reader = csv.DictReader(open(os.path.join(data_path, 'train_October_9_2012.csv')))
-# post_times = [int(time.mktime(dateparser.parse(datum['PostCreationDate']).timetuple()))
-#               for datum in reader]
-# post_times = np.array(post_times)
-# pickle.dump(post_times, open(full_path("post_times.p"), "wb"),
-#             protocol=pickle.HIGHEST_PROTOCOL)
-post_times = np.array(pickle.load(open(full_path("post_times.p"), "rb")))
-sort_arg = np.argsort(post_times)
+reader = csv.DictReader(open(os.path.join(data_path, 'train.csv')))
+post_times = [int(time.mktime(dateparser.parse(datum['PostCreationDate']).timetuple()))
+              for datum in reader]
+post_times = np.array(post_times)
+pickle.dump(post_times, open(full_path("post_times_37.p"), "wb"),
+            protocol=pickle.HIGHEST_PROTOCOL)
+post_times = np.array(pickle.load(open(full_path("post_times_37.p"), "rb")))
 
+sort_arg = np.argsort(post_times)
 
 def is_sorted(l):
     return all(l[i] <= l[i+1] for i in xrange(len(l)-1))
@@ -28,10 +28,10 @@ def is_sorted(l):
 print(is_sorted(post_times))
 print(is_sorted(post_times[sort_arg]))
 
-(train_xs, train_ys) = pickle.load(open(full_path("train_xs_ys_np.p"), "rb"))
+(train_xs, train_ys) = pickle.load(open(full_path("train_37_xs_ys_np.p"), "rb"))
 train_xs = train_xs[sort_arg]
 train_ys = train_ys[sort_arg]
 pickle.dump((train_xs, train_ys),
-            open(full_path("train_xs_ys_np_sorted.p"), "wb"),
+            open(full_path("train_37_xs_ys_np_sorted.p"), "wb"),
             protocol=pickle.HIGHEST_PROTOCOL)
 
